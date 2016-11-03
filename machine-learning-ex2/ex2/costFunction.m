378cbdac8e0075e1a98d1555d334a0f4c69ba0d9
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-11-03 13:52:55 -0500

    feat: Implement cost function and gradient calculation
    ex2: Logistic regression

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,10 +19,14 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
-
-
-
-
+% compute hypothesis
+h = sigmoid(X*theta)
+% compute number of training set samples
+m=size (X,1)
+% compute cost function
+J = 1/m.*(-y'* log (h) - (1 - y)'*log(1-h))
+% gradient function
+grad = 1/m.*X'*(h-y)
 
 
 
