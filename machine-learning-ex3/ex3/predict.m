0553acf9a8698f900a6942885607fd5315463cf0
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-11-10 13:40:22 -0600

    feat: ex3 - Implement neural network prediction function

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,13 +21,20 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
 
 
 
+z2 = (Theta1*X')';
+a2 = sigmoid(z2);
 
+a2 = [ones(size(a2,1),1) a2];
 
+z3 = (Theta2*a2')';
+a3 = sigmoid(z3);
 
-
+[max_values, p] = max(a3, [], 2);
 
 % =========================================================================
 
