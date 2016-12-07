de5ea91c4a53c87e5090cd9e3947ec88f69b2454
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-06 18:23:37 -0600

    feat: ex6 - Implement C & sigma optimization vs cross validation set

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% test values array for C and sigma
+test_values = [.01, .03, .1, .3, 1, 3, 10, 30];
+
+% Set prediction error  starting point above
+prediction_error_final = 1000;
 
 
+for C = test_values
+	for sigma = test_values
+		% Train model using training set and particular value of C and sigma:
+		model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+		% Make predictions with this model
+		predictions = svmPredict(model, Xval);
+		prediction_error = mean(double(predictions ~= yval));
+		if (prediction_error < prediction_error_final)
+			%printf("Found a new best case");
+			prediction_error_final = prediction_error;
+			C_best = C;
+			sigma_best = sigma;
+		end
+		%printf(strcat("Prediction error: C: ", num2str(C), " Sigma: ", num2str(sigma), " = ", num2str(prediction_error)));
+	end
+end
 
-
-
+C= C_best;
+sigma = sigma_best;
 
 % =========================================================================
 
