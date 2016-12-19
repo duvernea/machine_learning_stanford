6cb7d85bdbb69e5cd72d96b87870be8a079b3f35
BRIAN DUVERNEAY <duverneay@gmail.com>
2016-12-19 11:35:10 -0600

    feat: ex8 - Implement Collaborative Filtering Gradients

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,24 +40,11 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-J =  .5*sum(sum(((X*Theta' - Y).*R).^2))
+J =  .5*sum(sum(((X*Theta' - Y).*R).^2));
 
+X_grad = ((X*Theta' - Y).*R)*Theta;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Theta_grad = ((X*Theta' - Y).*R)'*X;
 
 % =============================================================
 
